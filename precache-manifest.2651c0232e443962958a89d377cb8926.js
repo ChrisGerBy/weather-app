c784a5957b7b7463d9d4c7b127c9c83fe33dfff8
Krystsina Gerko <k.gerko007@gmail.com>
2020-01-04 16:51:07 +0300

    Updates

---
+++ precache-manifest.2651c0232e443962958a89d377cb8926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678532eec24a3585bd53a057414450b2",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c38b15e2d6b298941cf6",
+    "url": "/weather-app/static/css/main.279cba46.chunk.css"
+  },
+  {
+    "revision": "3b6e7ef7df4fc33f807f",
+    "url": "/weather-app/static/js/2.b4d30fa9.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/weather-app/static/js/2.b4d30fa9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c38b15e2d6b298941cf6",
+    "url": "/weather-app/static/js/main.cc9feddd.chunk.js"
+  },
+  {
+    "revision": "06bb25e49de1386edcac",
+    "url": "/weather-app/static/js/runtime-main.7fed549c.js"
+  }
+]);
